2f14d5296dd3e1c73b3c2ae2dc65bc9b1d655e17
alirdn <alireza.dabirinejad@live.com>
2018-07-17 00:28:47 +0430

    Bump windows-kill version to 1.1.3

--- windows-kill/windows-kill.cpp
+++ windows-kill/windows-kill.cpp
@@ -14,7 +14,7 @@ using WindowsKillLibrary::sendSignal;
 using WindowsKillLibrary::SIGNAL_TYPE_CTRL_C;
 using WindowsKillLibrary::SIGNAL_TYPE_CTRL_BREAK;
 
-#define WINDOWSKILL_VERSION "1.1.1"
+#define WINDOWSKILL_VERSION "1.1.3"
 
 int main(int argc,char *argv[])
 {
